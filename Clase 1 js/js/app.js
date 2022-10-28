adc13c892bffc7e231faa6c4b6c8a470a2b10596
Alejandro Busi <Alejandrobusi@gmail.com>
2022-10-27 21:26:00 -0300

    subiendo segunda clase

---
+++ Clase 1 js/js/app.js
@@ -0,0 +1,91 @@
+// console.log( 30 + 20 );
+// //alert('Hola Banda');
+
+// let comision = '25i';
+// let edad = 18;
+// let estado = true;
+
+// console.log( typeof(estado) );
+
+// let indefinido;
+// let nula = null;
+// console.log(indefinido, nula);
+
+// var nombre;
+// let nombre2;
+// console.log(nombre2);
+
+// nombre2 = "ivan";
+
+// console.log(nombre2);
+
+// nombre2 = 'rodo';
+
+// console.log(nombre2);
+
+// const modelo = 2020;
+// console.log(modelo);
+
+
+// // Operador de asignacion
+
+// const asignarme = 'un valor';
+
+// // Operadores aritmeticos
+
+// console.log( 2 * 2);
+
+// console.log( 2 / 2 );
+
+// console.log( 50 % 8 );
+
+// // Operadores Unitarios
+// let incrementar = 1;
+// ++incrementar;
+// incrementar++;
+
+// console.log(incrementar);
+
+// let decrementar = 1;
+// decrementar--;
+// decrementar--;
+
+// console.log(decrementar);
+
+// let estadoOnOff = true;
+// console.log(!estadoOnOff);
+
+//Operadores relacionales
+
+console.log( 1 == 1 );
+
+console.log( 1 != 2 );
+
+console.log( 11 === '11');
+
+console.log( 11 !== '11' );
+
+console.log( 12 > 111 );
+
+console.log( 12 < 121 );
+
+console.log( 13 >= 131 );
+
+console.log( 14 <= 15 );
+
+// Operadores logicos
+
+//                   AND
+console.log( !(11 >= 12) && 3 == '3' );
+
+//                    OR
+console.log( 11 >= 11 || 3 == '3' );
+
+//                     NOT
+console.log(`este es ${!(11 > 12)}`);
+
+
+const borrar = 'eliminado';
+const añadir = 'añadir item';
+
+console.log( añadir );
---
+++ Clase 2/js/app.js
@@ -0,0 +1,132 @@
+// console.log('hello');
+
+// let people = 'Alvarito';
+
+// let incrementar = 1;
+// incrementar--;
+// console.log(incrementar);
+
+// const onOff = true;
+
+// console.log(!onOff);
+
+// console.log( 2 === 2 );
+
+// console.log( 2 !== '2' );
+
+// console.log( 10 >= 10 );
+
+// const email = true;
+// const pass = true;
+
+// console.log( !(email == true) && pass == true );
+
+// console.log( email == true || pass == true );
+
+// const defaultAge = 18;
+// const defaultCash = 20000;
+
+// const ageRodo = prompt('Ingrese su edad') || defaultAge;
+// const walletRodo = prompt('Ingrese su money') || defaultCash;
+// const minAge = 18;
+// const minWallet = 20000;
+
+// // if ( ageRodo === minAge && walletRodo === minWallet ) {
+// //   alert('Pasa Capo total!!');
+// // };
+
+// console.log('Estoy despues de el IF');
+
+// // CONDICIONAL DOBLE
+
+// if ( ageRodo >= minAge && walletRodo >= minWallet ) {
+//   alert('Pasa Capo total!!');
+// } else {
+//   alert('No pasas creckk, a la casita');
+// };
+
+// CONDICIONAL multiple o anidada
+// const num = 10;
+
+// if (num < 10) {
+//   alert('el numero es menor que 10');
+// } else {
+//   if (num > 10) {
+//     alert('el numero es mayor que 10');
+//   } else {
+//     if (typeof(num) == 'string') {
+//       alert('el dato ingresado no es un numero');
+//     } else {
+//       alert('el numero es igual a 10');
+//     }
+//   }
+// };
+
+//     CONDICIONAL multiple else if
+
+const num2 = "0";
+
+// if (num2 < 10) {
+//   alert('el numero es menor que 10');
+// } else if (num2 > 10) {
+//   alert('el numero es mayor que 10');
+// } else if (typeof(num2) == 'string') {
+//   alert('el dato ingresado no es un numero');
+// } else {
+//   alert('el numero es igual a 10');
+// };
+
+//       SWITCH
+
+// const day = prompt("Ingresa el dia");
+
+// switch (day) {
+//   case 'lunes':
+//     console.log('uff, ya es lunes de nuevo');
+//     break;
+//   case 'martes':
+//     console.log('Es martes, no esta tan mal');
+//     break;
+//   case 'miercoles':
+//     console.log('Es miercoles, ya me siento energico');
+//     break;
+//   case 'jueves':
+//     console.log('y si acortamos la semana?');
+//     break;
+//   case 'viernes':
+//     console.log('no debi hacer eso');
+//     break;
+//   case 'sabado':
+//     console.log('se sale de nuevo');
+//     break;
+//   case 'domingo':
+//     console.log('Porfa, un alikal');
+//     break;
+
+//   default:
+//     console.log('esto no es un dia');
+//     break;
+// }
+
+//     While
+let i = 101;
+
+// while (i <= 100) {
+//   console.log('Hola mundo');
+//   i++;
+// };
+
+// Do While
+
+// let contador = 111;
+
+// do {
+//   console.log('Hola mundo');
+//   contador++;
+// } while (contador <= 100);
+
+//   For
+
+for (let indice = 100; indice >= 1; indice--) {
+  console.log('estas en el for');
+};
